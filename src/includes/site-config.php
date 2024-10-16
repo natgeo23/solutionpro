0fe01b54b4d71449a2a6bf7829b94bd01ffd6158
natGEO <natgeo.ar@gmail.com>
2024-10-16 15:12:58 -0300

    Fix structured data

--- src/includes/site-config.php
+++ src/includes/site-config.php
@@ -1,20 +1,20 @@
 <?php
 // Dominio del sitio web (debe ir sin / al final)
-$site_domain = "https://www.tudominio.com";
+$site_domain = "https://info.mysportsession.com";
 
 // Nombre del sitio web
-$site_name = "My Sport Session";
+$site_name = "My Sport Session Pro";
 
 // Autor del sitio web
-$site_author = "Cabeza de Raton";
+$site_author = "https://union-atlantica.com";
 
 // Título del sitio web
-$site_title = "My Sport Session";
+$site_title = "My Sport Session Pro";
 
 // Descripción del sitio web
 $site_description = "Libérez vos journées et développez votre business avec My Sport Session. Gagnez en visibilité et optimisez la gestion de vos réservations.";
 
 // Palabras clave del sitio web
-$site_keywords = "lorem, ipsum, dolor, sit";
+$site_keywords = "Gestion de réservations, Optimisation des réservations, Développement de business, Visibilité en ligne, Plateforme de sport, Réservations en ligne, Gestion du temps, Augmenter la visibilité, Sport en ligne, Solution de gestion";
 
 ?>
